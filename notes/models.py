7bb13e00e2eb63e3e480dba5503a4875a5d478ea
Rommel Rico <me@rommelrico.com>
2019-04-05 21:06:51 -0700

    Typo

--- notes/models.py
+++ notes/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Notes(models.Model):
+class Note(models.Model):
     title = models.CharField(max_length=150)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
