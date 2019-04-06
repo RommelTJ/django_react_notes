71b3d38e8f82f62b9d77d60194e4ef0a8935d5bb
Rommel Rico <me@rommelrico.com>
2019-04-06 16:06:50 -0700

    Setting up routing

--- django_react_notes/routing.py
+++ django_react_notes/routing.py
@@ -1,5 +1,13 @@
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+
+from notes import routing
+
 
 application = ProtocolTypeRouter({
-    # Empty for now (http->django views is added by default)
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            routing.websocket_urlpatterns
+        )
+    )
 })
